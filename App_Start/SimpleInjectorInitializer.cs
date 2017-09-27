3a0b3515757ebdb48c4814fd43cbc37248845932
Vitalie Serdesniuc <vitalie.serdesniuc@expertnetwork.ro>
2017-09-27 19:02:22 +0300

    refactoring step 2

---
+++ App_Start/SimpleInjectorInitializer.cs
@@ -0,0 +1,36 @@
+﻿[assembly: WebActivator.PostApplicationStartMethod(typeof(LC_02.App_Start.SimpleInjectorInitializer), "Initialize")]
+
+namespace LC_02.App_Start
+{
+    using System.Reflection;
+    using System.Web.Mvc;
+
+    using SimpleInjector;
+    using SimpleInjector.Integration.Web;
+    using SimpleInjector.Integration.Web.Mvc;
+    
+    public static class SimpleInjectorInitializer
+    {
+        /// <summary>Initialize the container and register it as MVC3 Dependency Resolver.</summary>
+        public static void Initialize()
+        {
+            var container = new Container();
+            container.Options.DefaultScopedLifestyle = new WebRequestLifestyle();
+            
+            InitializeContainer(container);
+
+            container.RegisterMvcControllers(Assembly.GetExecutingAssembly());
+            
+            container.Verify();
+            
+            DependencyResolver.SetResolver(new SimpleInjectorDependencyResolver(container));
+        }
+     
+        private static void InitializeContainer(Container container)
+        {
+            LC_02.Services.Infrastucture.SimpleInjectorInitializer.InitializeContainer(container, Lifestyle.Scoped);
+            // For instance:
+            // container.Register<IUserRepository, SqlUserRepository>(Lifestyle.Scoped);
+        }
+    }
+}
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,14 +2,25 @@
 using System.Collections.Generic;
 using System.Data.Entity.Migrations;
 using System.Web.Mvc;
+using LC_02.Services.Infrastucture;
+using LC_02.Services.Students;
 
 namespace LC_02.Controllers
 {
     public class HomeController : Controller
     {
+
+        private readonly IStudentService studentService;
+        public HomeController(IStudentService studentService)
+        {
+            this.studentService = studentService;
+        }
         public ActionResult Index()
         {
-            
+            var studentDto = new StudentDto();
+            studentDto.Email = "addedEmail";
+            studentDto.FirstName = "addedFirstName";
+            studentService.AddNewStudent(studentDto);
             return View();
         }
 
